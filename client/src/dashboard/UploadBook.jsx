2197db86d38712ad509d54222cdaba0a8dacfd83
Pasindu Sankalpa <105929988+Pasindu599@users.noreply.github.com>
2024-05-26 19:26:29 +0530

    update uploadbook page

--- client/src/dashboard/UploadBook.jsx
+++ client/src/dashboard/UploadBook.jsx
@@ -1,7 +1,41 @@
 import React from "react";
 
+import { Button, Checkbox, Label, TextInput } from "flowbite-react";
+
 function UploadBook() {
-  return <div>UploadBook</div>;
+  return (
+    <div className="px-4 my-12 ">
+      <h2 className="mb-8 text-3xl font-bold">Upload A Book</h2>
+      <form className="flex lg:w-[1180px] flex-col flex-wrap gap-4">
+        <div className="flex gap-8">
+          <div className="lg:w-1/2">
+            <div className="mb-2 block">
+              <Label htmlFor="title" value="Book Title" />
+            </div>
+            <TextInput
+              id="bookTitle"
+              name="bookTitle"
+              type="text"
+              placeholder="Book title"
+              required
+            />
+          </div>
+          <div className="lg:w-1/2">
+            <div className="mb-2 block">
+              <Label htmlFor="title" value="Book Title" />
+            </div>
+            <TextInput
+              id="bookTitle"
+              name="bookTitle"
+              type="text"
+              placeholder="Book title"
+              required
+            />
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default UploadBook;
